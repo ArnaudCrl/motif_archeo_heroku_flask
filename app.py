85b6898948ff3780c6341d5fd00d02ab23fb7788
ArnaudCrl <44412756+ArnaudCrl@users.noreply.github.com>
2020-10-28 17:11:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,25 @@ from flask import Flask, jsonify, Response, request
 # from io import BytesIO
 from fastai.vision.all import *
 
-app = Flask(__name__)
+
 
 path = Path(__file__).parent
 
-async def download_file(url, dest):
+def download_file(url, dest):
     if dest.exists(): return
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
             with open(dest, 'wb') as f: f.write(data)
 
-async def setup_learner():
+def setup_learner():
     await download_file(model_file_url, model_path/model_file_name)
     learn = load_learner(model_path / model_file_name)
     return learn
 
+learn = setup_learner()
 
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -31,7 +33,7 @@ def index():
 @app.route('/analyze', methods=['POST'])
 async def analyze():
     print("appel fct analyse")
-    data = await request.form()
+    data = await request.file()
     img_bytes = await (data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction, _, values = learn.predict(img)
