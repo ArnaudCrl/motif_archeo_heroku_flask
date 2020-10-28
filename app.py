8c3e91cd2e9b8c81a13e9b1765ecd5648459341e
ArnaudCrl <44412756+ArnaudCrl@users.noreply.github.com>
2020-10-28 16:28:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,22 @@ from flask import Flask, jsonify, Response
 from fastai.vision.all import *
 
 app = Flask(__name__)
+
 path = Path(__file__).parent
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f: f.write(data)
+
+async def setup_learner():
+    await download_file(model_file_url, model_path/model_file_name)
+    learn = load_learner(model_path / model_file_name)
+    return learn
+
+
 
 @app.route('/')
 def index():
