aeb3143fd2d48a11b24df3a84179a7523fc43339
ArnaudCrl <44412756+ArnaudCrl@users.noreply.github.com>
2020-11-02 11:22:46 +0100

    Update show_result.js

--- static/js/show_result.js
+++ static/js/show_result.js
@@ -4,3 +4,10 @@ var el = x => document.getElementById(x);
 var dataImage = localStorage.getItem('imgData');
 saved_image = el('saved_image');
 saved_image.src = "data:image/jpeg;base64," + dataImage;
+
+import constants from './const.js';
+
+console.log(constants.ANOTHER_VARIABLE);
+console.log(constants.LISTING_TYPE_BRAND);
+console.log(constants.DAYS);
+console.log(constants.MAP_CONTENT_TYPE_TO_EXTENSION);
